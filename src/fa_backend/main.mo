fbb8b03ec893d5f7755ecc3b8ecce1f0c5fef4ce
theo <thehotelcar@gmail.com>
2024-04-24 10:53:44 -0700

    the rest

--- src/fa_backend/main.mo
+++ src/fa_backend/main.mo
@@ -1,5 +1,32 @@
+import HashMap "mo:base/HashMap"; 
+import Text "mo:base/Text";
+import Principal "mo:base/Principal";
+import Result "mo:base/Result";
+
+
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+
+  let members = HashMap.HashMap<Principal, Text>(0, Principal.equal, Principal.hash);
+
+    public query (message) func greet() : async Text {
+    return "Hello, " # Principal.toText(message.caller) # "!";
+    
   };
+
+
+  // public shared func addMember(member : Principal) : async Result<Text, Text> {
+  //     switch(members.get(caller)){
+  //         case(null){
+  //             members.put(caller, member);
+  //             return #ok();
+  //         };
+  //         case(? oldMember){
+  //             return #err("Your principal already exists")
+  //         };
+  //     };
+  //     return #err("Not implemented");
+  // };
+
+
+
 };
--- src/fa_frontend/src/App.jsx
+++ src/fa_frontend/src/App.jsx
@@ -1,17 +1,73 @@
 import { useState } from 'react';
-import { fa_backend } from 'declarations/fa_backend';
+import {fa_backend, createActor} from 'declarations/fa_backend';
+import {AuthClient} from "@dfinity/auth-client"
+import {HttpAgent} from "@dfinity/agent";
+// const path = require("path");
 
 function App() {
   const [greeting, setGreeting] = useState('');
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    fa_backend.greet(name).then((greeting) => {
+  // function initCanisterEnv() {
+  //   let localCanisters, prodCanisters;
+  //   try {
+  //     localCanisters = require(path.resolve('/.dfx/local/canister_ids.json'))
+  //   } catch (error) {
+  //     console.log("No local canister_ids.json found. Continuing production");
+  //   }
+  //   try {
+  //     prodCanisters = require(path.resolve("canister_ids.json"));
+  //   } catch (error) {
+  //     console.log("No production canister_ids.json found. Continuing with local");
+  //   }
+
+  //   // const canisterConfig = network === "local" ? localCanisters : prodCanisters;
+
+  //   const canisterConfig = localCanisters;
+
+  //   return Object.entries(canisterConfig).reduce((prev, current) => {
+  //     const [canisterName, canisterDetails] = current;
+  //     prev[canisterName.toUpperCase() + "_CANISTER_ID"] =
+  //       canisterDetails[network];
+  //     return prev;
+  //   }, {});
+  // }
+    
+  // const canisterEnvVariables = initCanisterEnv();
+  // const internetIdentityUrl = network === "local" ? `http://${canisterEnvVariables["INTERNET_IDENTITY_CANISTER_ID"]}.localhost:4943/` : `https://identity.ic0.app`
+
+    
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      const greeting = await fa_backend.greet();
       setGreeting(greeting);
-    });
-    return false;
+    } catch {
+      console.error('Error fetching greeting:', error);
+    }
   }
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    console.log(process.env.FA_BACKEND_CANISTER_ID)
+
+    try {
+      let authClient = await AuthClient.create();
+      await new Promise((resolve) => {
+        authClient.login({
+          identityProvider: "http://be2us-64aaa-aaaaa-qaabq-cai.localhost:4943/",
+          onSuccess: resolve,
+        });
+      });
+      const identity = authClient.getIdentity();
+      const agent = new HttpAgent({ identity });
+      actor = await createActor('bkyz2-fmaaa-aaaaa-qaaaq-cai', {
+        agent,
+      });
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+
+  } 
 
   return (
     <main>
@@ -22,6 +78,9 @@ function App() {
         <label htmlFor="name">Enter your name: &nbsp;</label>
         <input id="name" alt="Name" type="text" />
         <button type="submit">Click Me!</button>
+      </form>
+      <form action="#" onSubmit={handleLogin}>
+        <button type="submit">Login</button>
       </form>
       <section id="greeting">{greeting}</section>
     </main>
